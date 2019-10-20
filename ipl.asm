8317e3d0a0d78ec32c5b1209b54fb45c3ca29ac6
TakumiHANAUE <tkmhana@gmail.com>
2019-10-20 16:56:37 +0900

    day3 harib00a

--- ipl.asm
+++ ipl.asm
@@ -33,25 +33,42 @@ entry:
     MOV     SS, AX
     MOV     SP, 0x7c00
     MOV     DS, AX
-    MOV     ES, AX
 
+; ディスクを読む
+
+    MOV     AX, 0x0820
+    MOV     ES, AX
+    MOV     CH, 0               ; シリンダ0
+    MOV     DH, 0               ; ヘッド0
+    MOV     CL, 2               ; セクタ2
+
+    MOV     AH, 0x02            ; ディスク読み込み
+    MOV     AL, 1               ; 1セクタ
+    MOV     BX, 0
+    MOV     DL, 0x00            ; Aドライブ
+    INT     0x13                ; ディスクBIOS呼び出し
+    JC     error
+
+fin:
+    HLT
+    JMP     fin
+
+error:
     MOV     SI, msg
+
 putloop:
     MOV     AL, [SI]
     ADD     SI, 1
     CMP     AL, 0
     JE      fin
     MOV     AH, 0x0e
-    MOV     BX, 15
+    MOV     BX, 15              ; カラーコード
     INT     0x10
     JMP     putloop
-fin:
-    HLT
-    JMP     fin
 
 msg:
     DB      0x0a, 0x0a
-    DB      "hello, world"
+    DB      "load error"
     DB      0x0a
     DB      0
 
