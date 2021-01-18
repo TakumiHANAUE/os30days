670b20e2c0d09910cfa4c0855c3a442f20d4f2e0
TakumiHANAUE <tkmhana@gmail.com>
2021-01-18 23:59:26 +0900

    day4 harib01a

--- bootpack.c
+++ bootpack.c
@@ -1,9 +1,17 @@
 
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
-    while(1)
+    int i;
+
+    for (i = 0; i < 0xaffff; i++)
+    {
+        write_mem8(i, 15); /* MOV BYTE [i],15*/
+    }
+
+    while (1)
     {
         io_hlt();
     }
