95985da2e075b893d8146d2f832e3818764c720c
TakumiHANAUE <tkmhana@gmail.com>
2021-09-10 21:54:20 +0900

    mod dsctbl.c

--- dsctbl.c
+++ dsctbl.c
@@ -35,7 +35,7 @@ void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, i
     }
     sd->limit_low       = limit & 0xffff;
     sd->base_low        = base & 0xffff;
-    sd -> base_mid      = (base >> 16) & 0xff;
+    sd->base_mid        = (base >> 16) & 0xff;
     sd->access_right    = ar & 0xff;
     sd->limit_high      = ((limit >> 16) & 0xff) | ((ar >> 8) & 0xf0);
     sd->base_high       = (base >> 24) & 0xff;
