da5b64a29420e89274bb6966fe1ebc686285b525
TakumiHANAUE <tkmhana@gmail.com>
2021-10-08 19:28:20 +0900

    day11 harib08g

--- sheet.c
+++ sheet.c
@@ -52,7 +52,7 @@ void sheet_setbuf(struct SHEET *sht, char *buf, int xsize, int ysize, int col_in
     return;
 }
 
-void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
 {
     int h, bx, by, vx, vy, bx0, by0, bx1, by1;
     char *buf, *vram = ctl->vram;
@@ -63,7 +63,7 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
     if (vy0 < 0) { vy0 = 0; }
     if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
     if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
-    for (h = 0; h <= ctl->top; h++)
+    for (h = h0; h <= ctl->top; h++)
     {
         sht = ctl->sheets[h];
         buf = sht->buf;
@@ -90,6 +90,7 @@ void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
             }
         }
     }
+    return;
 }
 
 void sheet_updown(struct SHEET *sht, int height)
@@ -119,6 +120,7 @@ void sheet_updown(struct SHEET *sht, int height)
                 ctl->sheets[h]->height = h;
             }
             ctl->sheets[height] = sht;
+            sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1); /* 新しい下じきの情報に沿って画面を描き直す */
         }
         else /* 非表示化 */
         {
@@ -132,8 +134,8 @@ void sheet_updown(struct SHEET *sht, int height)
                 }
             }
             ctl->top--; /* 表示中の下じきが1つ減るので、一番上の高さが減る */
+            sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0); /* 新しい下じきの情報に沿って画面を描き直す */
         }
-        sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize); /* 新しい下じきの情報に沿って画面を描き直す */
     }
     else if (old < height) /* 以前よりも高くなる */
     {
@@ -157,7 +159,7 @@ void sheet_updown(struct SHEET *sht, int height)
             ctl->sheets[height] = sht;
             ctl->top++; /* 表示中の下敷きが1つ増えるので、一番上の高さが増える */
         }
-        sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize); /* 新しい下じきの情報に沿って画面を描き直す */
+        sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height); /* 新しい下じきの情報に沿って画面を描き直す */
     }
     return;
 }
@@ -166,7 +168,7 @@ void sheet_refresh(struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 {
     if (sht->height >= 0) /* もし表示中なら、新しい下じきの情報に沿って画面を描き直す */
     {
-        sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+        sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1, sht->height);
     }
     return;
 }
@@ -178,8 +180,8 @@ void sheet_slide(struct SHEET *sht, int vx0, int vy0)
     sht->vy0 = vy0;
     if (sht->height >= 0) /* もし表示中なら、新しい下じきの情報に沿って画面を描き直す */
     {
-        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
-        sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0);
+        sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height);
     }
     return;
 }
