ab2de43ae4a9761f74e79687dd3f2754788ffa48
TakumiHANAUE <tkmhana@gmail.com>
2021-10-19 20:57:35 +0900

    mod comment

--- fifo.c
+++ fifo.c
@@ -14,7 +14,7 @@ void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf)
     return;
 }
 
-/* FIFOへでーたを送り込んで蓄える */
+/* FIFOへデータを送り込んで蓄える */
 int fifo8_put(struct FIFO8 *fifo, unsigned char data)
 {
     if (fifo->free == 0)
