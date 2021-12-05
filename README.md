63c760b4f5387e77f2214b5e0b42af5a73972123
TakumiHANAUE <tkmhana@gmail.com>
2021-12-05 13:37:49 +0900

    day17 harib14g

--- README.md
+++ README.md
@@ -1512,3 +1512,5 @@ QEMU 上で測定したためばらつきが大きく、性能が上がったと
 - 書籍に従って `bootpack.c` を修正する（`projects/17_day/harib14f/bootpack.c`を参照する）
 
 #### Lock キー対応 (harib14g)
+
+- 書籍に従って `bootpack.c` を修正する（`projects/17_day/harib14g/bootpack.c`を参照する）
--- bootpack.c
+++ bootpack.c
@@ -8,15 +8,17 @@ void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void make_wtitle8(char *buf, int xsize, char *title, char act);
 void console_task(struct SHEET * sheet);
 
+#define KEYCMD_LED 0xed
+
 void task_b_main(struct SHEET *sht_back);
 
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
-    struct FIFO32 fifo;
     struct SHTCTL *shtctl;
     char s[40];
-    int fifobuf[128];
+    struct FIFO32 fifo, keycmd;
+    int fifobuf[128], keycmd_buf[32];
     int mx, my, i, cursor_x, cursor_c;
     unsigned int memtotal;
     struct MOUSE_DEC mdec;
@@ -45,7 +47,7 @@ void HariMain(void)
           0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
           0,   0,   0, '_',   0,   0,   0,   0,   0,   0,   0,   0,   0, '|',   0,   0,
     };
-    int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7;;
+    int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
 
     init_gdtidt();
     init_pic();
@@ -57,6 +59,7 @@ void HariMain(void)
     enable_mouse(&fifo, 512, &mdec);
     io_out8(PIC0_IMR, 0xf8); /* PITとPIC1とキーボードを許可(11111000) */
     io_out8(PIC1_IMR, 0xef); /* マウスを許可(11101111) */
+    fifo32_init(&keycmd, 32, keycmd_buf, 0);
 
     memtotal = memtest(0x00400000, 0xbfffffff);
     memman_init(memman);
@@ -126,8 +129,19 @@ void HariMain(void)
             memtotal / (1024 * 1024), memman_total(memman) / 1024);
     putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
 
+    /* 最初にキーボードの状態との食い違いがないように、設定しておくことにする */
+    fifo32_put(&keycmd, KEYCMD_LED);
+    fifo32_put(&keycmd, key_leds);
+
     while (1)
     {
+        if (fifo32_status(&keycmd) > 0 && keycmd_wait < 0)
+        {
+            /* キーボードコントローラに送るデータがあれば、送る */
+            keycmd_wait = fifo32_get(&keycmd);
+            wait_KBC_sendready();
+            io_out8(PORT_KEYDAT, keycmd_wait);
+        }
         io_cli();
         if (fifo32_status(&fifo) == 0)
         {
@@ -230,6 +244,33 @@ void HariMain(void)
                 if (i == 256 + 0xb6) /* 右シフト OFF*/
                 {
                     key_shift &= ~2;
+                }
+                if (i == 256 + 0x3a) /* CapsLock */
+                {
+                    key_leds ^= 4;
+                    fifo32_put(&keycmd, KEYCMD_LED);
+                    fifo32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0x45) /* NumLock */
+                {
+                    key_leds ^= 2;
+                    fifo32_put(&keycmd, KEYCMD_LED);
+                    fifo32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0x46) /* NumLock */
+                {
+                    key_leds ^= 1;
+                    fifo32_put(&keycmd, KEYCMD_LED);
+                    fifo32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0xfa) /* キーボードがデータを無事に受け取った */
+                {
+                    keycmd_wait = -1;
+                }
+                if (i == 256 + 0xfe) /* キーボードがデータを無事に受け取れなかった */
+                {
+                    wait_KBC_sendready();
+                    io_out8(PORT_KEYDAT, keycmd_wait);
                 }
                 /* カーソルの再表示 */
                 boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
