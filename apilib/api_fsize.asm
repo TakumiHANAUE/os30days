a8015021037708b618871b08f60b55eabb34195a
TakumiHANAUE <tkmhana@gmail.com>
2022-01-01 00:32:16 +0900

    mod api_fsize.asm

--- apilib/api_fsize.asm
+++ apilib/api_fsize.asm
@@ -8,4 +8,5 @@ api_fsize:                      ; int api_fsize(int fhandle, int mode);
     MOV     EDX, 24
     MOV     EAX, [ESP+4]        ; fhandle
     MOV     ECX, [ESP+8]        ; mode
+    INT     0x40
     RET
